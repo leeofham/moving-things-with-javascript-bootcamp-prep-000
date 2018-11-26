0891ac2e1afa682672f26478b867178498a12629
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-26 14:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 var dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > 0) {
+    dodger.style.left = `${right - 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 65) {
+    moveDodgerLeft()
+  }
+  if (e.which === 68) {
+    moveDodgerRight()
+  }
+})
